d851c81c84f82a0a0cd7b552254092c3b701f8e3
Chuying Huo <chuo8548@gmail.com>
2024-07-26 13:24:02 -0400

    Initial Commit

---
+++ kwkfinalproject/kwkfinalprojectApp.swift
@@ -0,0 +1,17 @@
+//
+//  kwkfinalprojectApp.swift
+//  kwkfinalproject
+//
+//  Created by Chuying Huo on 2024-07-26.
+//
+
+import SwiftUI
+
+@main
+struct kwkfinalprojectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
